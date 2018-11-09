bf4116892956e3614e6d40ce898e1a8a96a0b74e
espoiravenir465 <lapis074@gmail.com>
2018-11-09 22:15:30 +0900

    7-3added

--- MY theme/header.php
+++ MY theme/header.php
@@ -10,6 +10,8 @@
 <meta name="viewport"
 content="width=device-width",initial-scale=1.0">
 
+<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
